ba66d8a8ba9eb213ea1a1ff3c4cfe52fa544631f
shino16 <masatosinokawa@gmail.com>
2023-04-02 22:44:07 +0100

    Remove '/Game State/' and duplicated scripts

--- Game State/Cell.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Cell
-{
-    // An integer of 0 represents an empty cell, -1 represents a mine, 9 represents a ?
-    public readonly int number;
-    public bool revealed;
-    public bool flagged;
-}
