5ccc12395fa4ce94725bbaca60072ee90f9c42cb
shino16 <masatosinokawa@gmail.com>
2023-04-03 20:52:18 +0100

    Add info button and message panel that shows up on click

---
+++ Assets/Info.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info : MonoBehaviour
+{
+    public Button infoButton;
+    public GameObject howToPlayText, howToPlayBackground;
+
+
+    void Start()
+    {
+        infoButton.onClick.AddListener(() =>
+            howToPlayText.SetActive(!howToPlayText.activeSelf));
+        infoButton.onClick.AddListener(() =>
+            howToPlayBackground.SetActive(!howToPlayBackground.activeSelf));
+    }
+}
